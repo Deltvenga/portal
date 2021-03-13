6529b641d859dfcc72aca0a4d5087f2af47ca9fe
Roman <petessod@gmail.com>
2021-03-14 00:21:51 +0500

    added icons and new TaskDialog

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import LeftMenu from "./LeftMenu/LeftMenu";
 import TaskScreen from "./TaskScreen/TaskScreen";
+import Fab from "@material-ui/core/Fab";
+import EditIcon from '@material-ui/icons/Edit';
+import TaskDialog from "./TaskScreen/TaskDialog/TaskDialog";
 
 
 export class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentScreen: 'taskScreen' // taskScreen, userProfile, usersList, shop, ...
+            currentScreen: 'taskScreen', // taskScreen, userProfile, usersList, shop, ...
+            isTaskDialogOpen: false
         }
         this.screenChanger.bind(this);
     }
@@ -52,6 +56,14 @@ export class App extends Component {
                 <div className="App-mainBlock">
                     {this.getCurrentScreen()}
                 </div>
+                <div className="App-fab">
+                    <Fab onClick={() => {this.setState({isTaskDialogOpen: true})}} color="secondary" aria-label="edit">
+                        <EditIcon />
+                    </Fab>
+                </div>
+                <TaskDialog
+                    isOpen={this.state.isTaskDialogOpen}
+                    close={() => {this.setState({isTaskDialogOpen: false})}}/>
             </div>
         );
     }
---
+++ client/src/TaskScreen/TaskDialog/TaskDialog.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import TextField from "@material-ui/core/TextField";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import "./TaskDialog.css"
+
+export class TaskDialog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    handleClose() {
+        this.props.close();
+    };
+
+    handleOpen() {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <Dialog open={this.props.isOpen} onClose={() => {this.handleClose()}} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Новая задача</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Опишите текст и суть задачи максимально лаконично
+                        </DialogContentText>
+                        <div className="App-TaskScreen-TaskDialog__InputBlock">
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                label="Заголовок задачи"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </div>
+                        <div className="App-TaskScreen-TaskDialog__InputBlock">
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Описание задачи"
+                                multiline
+                                rows={4}
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </div>
+                        <div className="App-TaskScreen-TaskDialog__InputBlock">
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Исполнитель"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button size="large" color="secondary" onClick={() => {this.handleClose()}}>
+                            Отмена
+                        </Button>
+                        <Button size="large" onClick={() => {this.handleClose()}} color="primary">
+                            Создать
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default TaskDialog;
