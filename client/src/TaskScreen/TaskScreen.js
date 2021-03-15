1095f095e8e6124816a0f09b568fdabe8a122756
Roman <petessod@gmail.com>
2021-03-15 22:37:15 +0500

    added server events middleware

--- client/src/TaskScreen/TaskScreen.js
+++ client/src/TaskScreen/TaskScreen.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 
 export class TaskScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            serverNumber: 0
         }
+    }
+
+    subscribeToEvents() {
+        const events = new EventSource('http://localhost:9000/events');
+        events.addEventListener('testMessage', (data) => {
+            this.setState({serverNumber: JSON.parse(data.data).number});
+        })
+    }
+
+    sendMessage() {
+        axios.get('http://localhost:9000/startEvent').then((res) => {
+            console.log(res.data)
+        })
     }
 
     render() {
         return (
             <div>
                 Тут будут задачи
+                <br/>Серверный таймер: {this.state.serverNumber}
+                <Button onClick={() => {this.subscribeToEvents()}} variant="contained">subscribe to events</Button>
+                <Button onClick={() => {this.sendMessage()}} variant="contained">start spam</Button>
             </div>
         );
     }
--- server.js
+++ server.js
@@ -4,8 +4,59 @@ const path = require('path');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+
+let clients = [];
+function eventsHandler(req, res, next) {
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    };
+    res.writeHead(200, headers);
+    const clientId = Date.now();
+    res.write(`event: newUser\nid: ${clientId}\ndata: ${JSON.stringify({id: clientId})}\n\n`)
+    const newClient = {
+        id: clientId,
+        res
+    };
+    clients.push(newClient);
+    req.on('close', () => {
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter(c => c.id !== clientId);
+    });
+}
+
+function sendDataToAll(data) {
+    clients.forEach((c) => {
+        c.res.write(`event: testMessage\nid: ${c.id}\ndata: ${JSON.stringify(data)}\n\n`)
+    });
+}
+
+function sendDataToUser(userId, data) {
+    clients.forEach((c) => {
+        if(c.id === userId) {
+            c.res.write(`event: testMessage\nid: ${userId}\ndata: ${JSON.stringify(data)}\n\n`)
+        }
+    });
+}
+
+function sendEventToUser(event, userId, data) {
+    clients.forEach((c) => {
+        if(c.id === userId) {
+            c.res.write(`event: ${event}\nid: ${userId}\ndata: ${JSON.stringify(data)}\n\n`)
+        }
+    });
+}
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+// Define endpoints
+app.get('/events', eventsHandler);
+app.get('/status', (req, res) => res.json({clients: clients.length}));
 
 var Schema = mongoose.Schema;
 mongoose.connect('mongodb+srv://admin:lazeradmin@cluster0.dkanu.mongodb.net/moneyKeeper', {
@@ -13,7 +64,7 @@ mongoose.connect('mongodb+srv://admin:lazeradmin@cluster0.dkanu.mongodb.net/mone
     useUnifiedTopology: true
 });
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 const GoodsModel = mongoose.model('goods', new Schema({
     _goodName: String,
     _cost: Number,
@@ -25,6 +76,12 @@ app.get('/getGoods', (req, res) => {
         console.log(result)
     });
 });
+
+let counter = 0;
+app.get('/startEvent', (req, res) => {
+    setInterval(() => {sendDataToAll({number: counter++})}, 1000);
+});
+
 
 const PORT = 9000;
 app.listen(PORT, () => console.log("server started"));
